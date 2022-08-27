8ef9e9573efe83f6b93994bcfc50945a88ee14d0
Hardik Aggarwal <71537080+csehardik@users.noreply.github.com>
2022-08-27 21:58:07 +0530

    Update Password_Generator.java

--- Password Generator/Password_Generator.java
+++ Password Generator/Password_Generator.java
@@ -1,1 +1,25 @@
+package PASSWORDGENERATOR;
+
+import java.util.Random;
+
+public class RANDOMPASSWORDGENERATOR {
+
+	public static void main(String[] args) {
+		String upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lower = "abcdefghijklmnopqrstuvwxyz";
+		String num = "0123456789";
+		String specialcharacters = "<> , .?/}]{]+_-)(*&^%$#@!=";
+		String combination = upper+lower+specialcharacters+num;
+		int passwordlength = 8;
+		char[] password = new char[passwordlength];
+		Random r = new Random();
+		for(int i=0;i<passwordlength;i++) {
+			password[i]=combination.charAt(r.nextInt(combination.length()));
+		}
+		
+		System.out.println("Generated Password Is : " + new String (password));
+		
+	}
+	
+}
 
